d2f5eec37cd4d5ad1909c1f068599d6311f7eae5
11qqqax <148884847+11qqqax@users.noreply.github.com>
2023-11-05 17:37:37 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,3 +4,16 @@ import sys
 
 print(sys.executable)
 print(sys.path)
+
+
+import os
+
+folder_path = r'D:\\mesh_data\\4\\outt'  # f g v
+
+for filename in os.listdir(folder_path):
+    # if filename.startswith('camel-') and filename.endswith('.obj'):
+    if filename.startswith('out_'):
+        new_name = filename.replace('out_', 'outt_')
+        old_path = os.path.join(folder_path, filename)
+        new_path = os.path.join(folder_path, new_name)
+        os.rename(old_path, new_path)
